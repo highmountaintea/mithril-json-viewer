0a352e3721ec37536c5145559546f7b431a9b08b
highmountaintea <jlpub@coolimage.com>
2019-01-22 18:13:40 -0500

    added sample

---
+++ sample/app.js
@@ -0,0 +1,85 @@
+function renderCollapsed(propStr, isArray, pathStr, statusLookup) {
+  return m('div', [
+    '' + propStr + (isArray ? '[' : '{'),
+    m('button', {
+      onclick: function () { statusLookup[pathStr] = false; },
+    }, '...'),
+    (isArray ? ']' : '}'),
+  ]);
+}
+
+function renderExpanded(propStr, tree, paths, isArray, pathStr, statusLookup, collapseAfter) {
+  var divs = [];
+  divs.push('' + propStr + (isArray ? '[' : '{'));
+  divs.push(
+    m('button', {
+      onclick: function () { statusLookup[pathStr] = true; },
+    }, '-')
+  );
+  for (var key in tree) {
+    if (!tree.hasOwnProperty(key)) continue;
+    var child = tree[key];
+    var newpath = paths.slice();
+    newpath.push(key);
+    divs.push(renderTree(key, child, newpath, statusLookup, collapseAfter));
+  }
+  divs.push((isArray ? ']' : '}'));
+  return m('div', divs);
+}
+
+function renderTree(prop, tree, paths, statusLookup, collapseAfter) {
+  var propStr = prop == null ? '' : prop + ':';
+  var pathStr = JSON.stringify(paths);
+  var collapsed = statusLookup[pathStr];
+  if (collapsed == null) {
+    if (paths.length > collapseAfter) collapsed = true;
+    else collapsed = false;
+  }
+  if (tree != null && tree instanceof Array) {
+    if (collapsed) return renderCollapsed(propStr, true, pathStr, statusLookup);
+    else return renderExpanded(propStr, tree, paths, true, pathStr, statusLookup, collapseAfter);
+  }
+  if (tree != null && typeof tree === 'object') {
+    if (collapsed) return renderCollapsed(propStr, false, pathStr, statusLookup);
+    else return renderExpanded(propStr, tree, paths, false, pathStr, statusLookup, collapseAfter);
+  }
+  return m('div', propStr + JSON.stringify(tree));
+}
+
+function JsonViewer(initialVnode) {
+  var attrs = initialVnode.attrs;
+  var tree = attrs.tree;
+  var options = attrs.options || {};
+  var statusLookup = {};
+
+  return {
+    view: function(vnode) {
+      return renderTree(null, tree, [], statusLookup, options.collapseAfter);
+    }
+  };
+};
+
+const tree = {
+  name: 'I am a tree',
+  children: [
+    { name: 'I am child 1' },
+    { name: 'I am child 2' },
+    {
+      name: 'I am child 3',
+      children: ['grand child 1']
+    },
+  ],
+  nested: {
+    opt1: true,
+    opt2: [1, 'a'],
+    opt5: { a: 1, b: 2 },
+  },
+};
+
+function view() {
+  return m('div', { class: 'json-tree',  style: 'font-family: Courier;' },
+    m(JsonViewer, { tree: tree, options: { collapseAfter: 1 } }),
+  );
+}
+
+m.mount(document.body, { view: view });
