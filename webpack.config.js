a1d8e1a0425420d7ed2181caed444969509cbb4e
highmountaintea <jlpub@coolimage.com>
2019-01-30 12:21:31 -0500

    now produces CDN as well

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const minify = process.env.MINIFY ? '.min' : '';
+
+let config = {
+  entry: {
+    [`cdn${minify}.js`]: ["./index.js"],
+  },
+  output: {
+    filename: "./[name]",
+    library: "JsonViewer",
+    libraryTarget: "window",
+  },
+  externals: {
+    mithril: "mithril",
+  },
+};
+
+if (minify === '') config.optimization = { minimize: false };
+else config.optimization = {
+  minimizer: [new UglifyJsPlugin()],
+};
+
+module.exports = config;
